d22fa9d286f78d838cb68d278d66b917896971a1
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-03 14:25:48 -0600

    emitting events completed

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -2,12 +2,19 @@
   <div>
     <!-- <h2>I'm a todo item</h2> -->
     <h2>{{ todo.name }}</h2>
+    <button @click="handleClick">Mark Completed</button>
   </div>
 </template>
 
 <script>
 export default {
   props: ["todo"],
+
+  methods: {
+    handleClick() {
+      this.$emit("status-change", this.todo);
+    },
+  },
 };
 </script>
 
